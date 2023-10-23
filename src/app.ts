af36bd7c914ad14b58348f09250ec05afca8cb09
rtrend96 <rtrend96@gmail.com>
2023-10-23 15:39:51 -0500

    GMA-1 added CSS

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+function searchAddress(event: Event) {
+	event.preventDefault();
+	const enteredAddress = addressInput.value;
+}
+form.addEventListener("submit", searchAddress);
